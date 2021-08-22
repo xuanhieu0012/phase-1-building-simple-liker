34af656100db95c2a215392e646c009a6dc62cd2
Hieu <xuanhieu0012@gmail.com>
2021-08-22 14:41:31 -0500

    Completed

--- main.js
+++ main.js
@@ -3,12 +3,51 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorMessageModal = document.getElementById('modal')
+const likeBtnLi = document.querySelectorAll(".like")
+const likeGylphSpan = document.querySelectorAll(".like-glyph")
+console.log(likeBtnLi)
+
+function hearMelting(){
+  
+  errorMessageModal.className = 'hidden'
+  mimicServerCall()
+  .then(heartEvent())
+  .catch(handlerErrorMes())
+
+
+}
+function heartEvent(){
+
+  likeBtnLi.forEach(ele => 
+    
+    ele.addEventListener('click', e =>{
+     
+    if (e.target.textContent === EMPTY_HEART){
+      e.target.className = "activated-heart"
+      return e.target.textContent = FULL_HEART
+  } else (e.target.textContent === FULL_HEART)
+      e.target.className = "emty"
+      return  e.target.textContent = EMPTY_HEART
+    
+  }))
+}
+
+function handlerErrorMes(){
+  errorMessageModal.className ="Show-Message"
+  setTimeout(function (){
+    errorMessageModal.className = "hidden"
+
+  }, 3000)
+}
+
+hearMelting()
 
 
 
 
 //------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
+// Don't change the code below: this f0unction mocks the server response
 //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
